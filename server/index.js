eab0c6fdc601ef79e240ae3d5eaadf7dc0a41f0f
puneeeeeeet <puneetchhabra1607@gmail.com>
2024-07-10 14:35:03 +0530

    cors origin

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const listingRoutes = require("./routes/listing.js")
 const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://tbm-frontend.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 app.use(express.static("public"));
 
